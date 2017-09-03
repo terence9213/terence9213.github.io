577db3fa5f10d902cc0ea1e0d6ffb1e1c350bef4
Terence Lim <terence9213@gmail.com>
2017-09-03 22:19:53 +0800

    First major commit
    Added basic CSS and JS files for bootstrap and JQuery.
    Organised file structure.

---
+++ js/common.js
@@ -0,0 +1,15 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/* -- PRE LOADER -- */
+
+$(window).on("load", function(){
+    setTimeout(function() {$("#preloader").fadeOut();}, 1000); 
+    //$("#preloader").fadeOut();
+    
+});
+
