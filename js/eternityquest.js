bcd4a32fdab7ff6026143c9847b85fe867d936cf
Terence Lim <terence9213@gmail.com>
2017-10-13 02:37:18 +0800

    ETERNITY QUEST
    First atempt at a game.
    All the basics of a verticle scroller shooter.

---
+++ js/eternityquest.js
@@ -0,0 +1,205 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var canvas = document.getElementById("game-canvas");
+var ctx = canvas.getContext("2d");
+
+canvas.width = 600;
+canvas.height = 800;
+
+var score = 0;
+
+//AVATAR 
+var avatarX = canvas.width/2;
+var avatarY = canvas.height/2;
+var avatarWidth = 20;
+var avatarHeight = 20;
+var avatarMs = 8; // movement speed in x/y
+
+//PROJECTILE
+var projectileArray = [];
+var projectileRadius = 5;
+var projectileClr = "red";
+var projectileDmg = 10;
+var projectiletMs = 20;
+//PROJECTILE OBJ
+function Projectile(){
+    this.x = avatarX + avatarWidth/2;
+    this.y = avatarY + avatarHeight/2;
+}
+
+//ENEMY
+var enemyArray = [];
+var lastSpawnTime = Date.now();
+var spawnInterval = 2000;
+var maxHealth = 1000;
+var minHealth = 100;
+//ENEMY OBJ
+function Enemy(x, y, width, height, colour, health, ms){
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.colour = colour;
+    this.health = health;
+    this.ms = ms;
+}
+
+//UI EVENTS
+var up = false;
+var down = false;
+var left = false;
+var right = false;
+var fire = false;
+
+draw();
+function draw(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    drawAvatar();
+    drawProjectiles();
+    drawEnemys();
+    drawScore();
+    
+    requestAnimationFrame(draw);
+}
+
+function drawAvatar(){
+    //BORDER COLLISION
+    if(avatarY <= 0){ up = false; }
+    if(avatarY >= canvas.height - avatarHeight){ down = false; }
+    if(avatarX <= 0){ left = false; }
+    if(avatarX >= canvas.width - avatarWidth){ right = false; }
+    
+    //POSITIONING
+    if(up){ avatarY -= avatarMs; }
+    if(down){ avatarY += avatarMs; }
+    if(left){ avatarX -= avatarMs; }
+    if(right){ avatarX += avatarMs; }
+    
+    ctx.beginPath();
+    ctx.rect(avatarX, avatarY, avatarWidth, avatarHeight);
+    ctx.fillStyle = "blue";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawProjectiles(){
+    if(fire){
+        projectileArray.push(new Projectile());
+    }
+    
+    for(var i = 0 ; i < projectileArray.length ; i++){
+        var p = projectileArray[i];
+        //BORDER COLLISION
+        if(p.y <= 0){
+            projectileArray.splice(i,1);
+        }
+        //POSITIONING
+        p.y -= projectiletMs;
+        //RENDER
+        ctx.beginPath();
+        ctx.arc(p.x, p.y, projectileRadius, 0, Math.PI*2, false);
+        ctx.fillStyle = projectileClr;
+        ctx.fill();
+        ctx.closePath();
+    }
+}
+
+function drawEnemys(){ //Yes I know
+    //Generate random enemy randomly
+    if(Date.now() >= lastSpawnTime + spawnInterval){
+        //SPAWN
+        var x = Math.floor(Math.random() *canvas.width);
+        var health = Math.floor(Math.random() * (maxHealth - minHealth) + minHealth);
+        var width = health / 10;
+        var height = Math.floor(width * 0.8);
+        enemyArray.push(new Enemy(x, 0, width, height, "yellow", health, 5));
+        lastSpawnTime = Date.now();
+    }
+    
+    for(var i = 0 ; i < enemyArray.length ; i++){
+        var e = enemyArray[i];
+        //BORDER COLLISION
+        if(e.y >= canvas.height){
+            enemyArray.splice(i,1);
+        }
+        //PROJECTILE COLLISIONS
+        else{
+            for(var j = 0 ; j < projectileArray.length ; j++){
+                var p = projectileArray[j];
+                if(p.x > e.x && p.x < e.x + e.width && p.y > e.y && p.y < e.y + e.height){
+                    e.health -= projectileDmg;
+                    projectileArray.splice(j,1);
+                    if(e.health <= 0){
+                        enemyArray.splice(i,1);
+                        score += 1;
+                    }
+                }
+            }
+        }
+        //POSITIONING
+        e.y += e.ms;
+        //RENDER
+        ctx.beginPath();
+        ctx.rect(e.x, e.y, e.width, e.height);
+        ctx.fillStyle = e.colour;
+        ctx.fill();
+        ctx.closePath();
+    }
+    
+}
+
+function drawScore(){
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Score: "+score, 8, 20);
+}
+
+//UI EVENT LISTENERS
+// up:38 down:40 left:37 right:39
+document.addEventListener("keydown", function(event){
+    //event.preventDefault();
+    switch(event.keyCode){
+        case 38:
+            up = true;
+            break;
+        case 40:
+            down = true;
+            break;
+        case 37:
+            left = true;
+            break;
+        case 39:
+            right = true;
+            break;
+        case 90:
+            fire = true;
+            break;
+    }
+});
+
+document.addEventListener("keyup", function(event){
+    //event.preventDefault();
+    //console.log(event.keyCode);
+    switch(event.keyCode){
+        case 38:
+            up = false;
+            break;
+        case 40:
+            down = false;
+            break;
+        case 37:
+            left = false;
+            break;
+        case 39:
+            right = false;
+            break;
+        case 90:
+            fire = false;
+            break;
+    }
+});
